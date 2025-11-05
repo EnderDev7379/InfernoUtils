e0eaa6d697d631493577e1d15463179986cef45e
ender <enderdev7379@gmail.com>
2025-11-05 12:50:46 +0100

    Redo Semi-Hardcore and add Combat Logging prevention

--- src/main/java/net/gooseman/inferno_utils/InfernoUtils.java
+++ src/main/java/net/gooseman/inferno_utils/InfernoUtils.java
@@ -3,9 +3,12 @@ package net.gooseman.inferno_utils;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.gooseman.inferno_utils.config.InfernoConfig;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
@@ -15,8 +18,8 @@ import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
-
 
 public class InfernoUtils implements ModInitializer {
 	public static final String MOD_ID = "inferno_utils";
@@ -26,13 +29,34 @@ public class InfernoUtils implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static HashMap<String, Long> playerCombatTracker = new HashMap<>();
+
+	public List<String> probableTraps = List.of(new String[]{"bad_respawn_point", "falling_anvil", "falling_stalactite", "fireworks", "stalagmite"});
+	public List<EntityType<?>> explosionExclusion = List.of(new EntityType<?>[]{EntityType.CREEPER, EntityType.GHAST, EntityType.ENDER_DRAGON, EntityType.WITHER, EntityType.END_CRYSTAL});
+
+	public void temporaryBan(PlayerEntity playerEntity, String timeKey, String reasonKey) {
+		MinecraftServer server = playerEntity.getServer();
+		String command = null;
+		try {
+			command = String.format("tempban %s %s %s", playerEntity.getDisplayName().getString(), InfernoConfig.config.getOrDefault(timeKey, "8h"), InfernoConfig.config.getOrDefault(reasonKey, "You have died/combat logged"));
+			server.getCommandSource().getDispatcher().execute(command, server.getCommandSource());
+		} catch (Exception e) {
+			playerEntity.sendMessage(Text.of("An error occured, please contact the server owner"), false);
+			LOGGER.error("Couldn't tempban player!");
+			LOGGER.error("Exception Message: {}", e.getMessage());
+			if (command != null) LOGGER.error("Executed Command: {}", command);
+		}
+	}
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		probableTraps = probableTraps.stream().map((str) -> "minecraft:"+str).toList();
 
+		InfernoConfig.reloadConfig();
 
 		UseItemCallback.EVENT.register((player, world, hand) -> {
 			if (!world.isClient && player.getGameMode() != GameMode.SPECTATOR && player.getStackInHand(hand).isOf(Items.FIREWORK_ROCKET)) {
@@ -41,32 +65,51 @@ public class InfernoUtils implements ModInitializer {
 			return ActionResult.PASS;
         });
 
-		ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
-			if (entity instanceof PlayerEntity playerEntity) {
-				String deathTypeId = damageSource.getTypeRegistryEntry().getIdAsString();
-				Entity attacker = damageSource.getAttacker();
-				List<String> banExclusions = List.of(InfernoConfig.getStringArray("ban_exclusions"));
-				if (InfernoConfig.config.getOrDefault("debug", true)) {
-                    LOGGER.debug("{} died by {}", playerEntity.getDisplayName().getString(), deathTypeId);
-                    LOGGER.debug("Attacker is {}", (attacker != null ? attacker.getDisplayName().getString() : "non existent"));
-					LOGGER.debug("Excluded deaths: {}", String.join(", ", banExclusions));
-					LOGGER.debug("Source is {}", (damageSource.getSource() != null ? damageSource.getSource().getDisplayName().getString() : "non existent"));
-				}
-				if ((attacker instanceof PlayerEntity || !banExclusions.contains(deathTypeId)) && attacker != entity) {
-					MinecraftServer server = playerEntity.getServer();
-					String command = null;
-                    try {
-						command = "tempban " + playerEntity.getDisplayName().getString() + " " + InfernoConfig.config.getOrDefault("ban_time", "10s") + " You have died! If this death was not caused by a player (either directly or via trap), or you think it was otherwise unfair, please contact the moderators through the #tickets discord channel.";
-                        server.getCommandSource().getDispatcher().execute(command, server.getCommandSource());
-                    } catch (Exception e) {
-						playerEntity.sendMessage(Text.of("An error occured, please contact the server owner"), false);
-						LOGGER.error("Couldn't tempban player on death!");
-                        LOGGER.error("Exception Message: {}", e.getMessage());
-						if (command != null) LOGGER.error("Executed Command: {}", command);
-                    }
-                }
+		ServerLivingEntityEvents.AFTER_DAMAGE.register((entity, source, baseDamageTaken, damageTaken, blocked) -> {
+			if (entity instanceof PlayerEntity victim && source.getAttacker() instanceof PlayerEntity attacker && victim != attacker) {
+				long currentTime = entity.getWorld().getTime();
+				playerCombatTracker.put(victim.getUuidAsString(), currentTime);
+				playerCombatTracker.put(attacker.getUuidAsString(), currentTime);
 			}
 		});
 
+		ServerPlayerEvents.LEAVE.register((player) -> {
+			InfernoConfig.reloadConfig();
+			String playerUuid = player.getUuidAsString();
+			long currentTime = player.getWorld().getTime();
+			Long lastCombatTime = playerCombatTracker.get(playerUuid);
+			if (lastCombatTime != null && currentTime - lastCombatTime <= InfernoConfig.config.getOrDefault("combat_length", 400)) {
+				temporaryBan(player, "combat_ban_time", "combat_ban_reason");
+			}
+		});
+
+		ServerLivingEntityEvents.AFTER_DEATH.register(((entity, damageSource) -> {
+			InfernoConfig.reloadConfig();
+			if (!(entity instanceof PlayerEntity player)) return;
+
+			String playerUuid = player.getUuidAsString();
+			long currentTime = player.getWorld().getTime();
+			Long lastCombatTime = playerCombatTracker.get(playerUuid);
+
+			Entity attacker = damageSource.getAttacker();
+			Entity source = damageSource.getSource();
+			String deathTypeId = damageSource.getTypeRegistryEntry().getIdAsString();
+
+			if (InfernoConfig.config.getOrDefault("debug", true)) {
+                LOGGER.warn("{} died by {}", player.getDisplayName().getString(), deathTypeId);
+                LOGGER.warn("Attacker is {}", (attacker != null ? attacker.getDisplayName().getString() : "non existent"));
+				LOGGER.warn("Source is {}", (source != null ? source.getDisplayName().getString() : "non existent"));
+			}
+
+			if (lastCombatTime != null && currentTime - lastCombatTime <= InfernoConfig.config.getOrDefault("combat_length", 400)) {
+				temporaryBan(player, "death_ban_time", "death_ban_reason");
+			} else if (probableTraps.contains(deathTypeId) ||
+					(deathTypeId.equals("minecraft:indirect_magic") && source.getType() == EntityType.SPLASH_POTION && attacker == null) ||
+					((deathTypeId.equals("minecraft:explosion") || deathTypeId.equals("minecraft:player_explosion")) && (attacker == null || (!explosionExclusion.contains(attacker.getType()) && !explosionExclusion.contains(source.getType()))))) {
+				temporaryBan(player, "death_ban_time", "death_ban_reason");
+			}
+		}));
+
+		ServerLifecycleEvents.SERVER_STOPPING.register((server) -> playerCombatTracker.clear());
 	}
 }
--- src/main/java/net/gooseman/inferno_utils/config/InfernoConfig.java
+++ src/main/java/net/gooseman/inferno_utils/config/InfernoConfig.java
@@ -9,18 +9,35 @@ public class InfernoConfig {
                     # Whether to show debug messages
                     debug=true
                     
-                    # Semi-Hardcore
+                    
+                    # Semi-Hardcore / Combat Logging Detection
+                    
+                    # How long being in combat lasts in ticks
+                    combat_length=400
                     
                     # How long the death ban should last
-                    ban_time=8h
+                    death_ban_time=8h
+                    # Reason for the death ban
+                    death_ban_reason=You have died! If this death was not caused by a player, or you think it was otherwise unfair, please contact the moderators through the #tickets discord channel.
                     
-                    # What Death/Damage Types shouldn't cause a ban, comma seperated list
+                    # How long the combat logging ban should last
+                    combat_ban_time=8h
+                    # Reason for the combat logging ban
+                    combat_ban_reason=Combat logging isn't permitted! If you weren't in combat, or you think this ban is otherwise unfair, please contact the moderators through the #tickets discord channel.
+                    
+                    # What Damage Types don't put the player in combat / cause a ban on death, comma seperated list
                     # For possible values and their explanations check out https://minecraft.wiki/w/Damage_type#List_of_damage_types
-                    # Append minecraft: everywhere
-                    ban_exclusions=[minecraft:sweet_berry_bush,minecraft:sting,starve,minecraft:spit,minecraft:sonic_boom,minecraft:outside_border,minecraft:out_of_world,minecraft:mob_projectile,minecraft:mob_attack_no_aggro,minecraft:mob_attack,minecraft:lightning_bolt,minecraft:hot_floor,minecraft:generic_kill,minecraft:generic,minecraft:freeze,minecraft:fly_into_wall,minecraft:fireball,minecraft:ender_pearl,minecraft:drown,minecraft:cramming,minecraft:campfire,minecraft:cactus,minecraft:arrow]
+                    # Append the namespace of the damage type (e.g. "minecraft:" for vanilla damage types) in front of the damage type
+                    combat_exclusions=[minecraft:sweet_berry_bush,minecraft:sting,starve,minecraft:spit,minecraft:sonic_boom,minecraft:outside_border,minecraft:out_of_world,minecraft:mob_projectile,minecraft:mob_attack_no_aggro,minecraft:mob_attack,minecraft:lightning_bolt,minecraft:hot_floor,minecraft:generic_kill,minecraft:generic,minecraft:freeze,minecraft:fly_into_wall,minecraft:fireball,minecraft:ender_pearl,minecraft:drown,minecraft:cramming,minecraft:campfire,minecraft:cactus,minecraft:arrow]
                     """;
     }
-    public static SimpleConfig config = SimpleConfig.of("inferno_utils-config").provider(InfernoConfig::provider).request();
+    public static SimpleConfig.ConfigRequest configRequest = SimpleConfig.of("inferno_utils-config").provider(InfernoConfig::provider);
+    public static SimpleConfig config = configRequest.request();
+
+    public static void reloadConfig() {
+        config = configRequest.request();
+        config.getOrDefault("debug", true);
+    }
 
     public static String[] getStringArray(String key) {
         String arrayString = config.getOrDefault(key, "[]");
